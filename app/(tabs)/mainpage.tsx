841516831e5e815d6d28c1c5807667b1d1a86f8c
Pascal <zagarolopascal@gmail.com>
2024-07-26 00:27:37 +0200

    inserat card v1

--- app/(tabs)/mainpage.tsx
+++ app/(tabs)/mainpage.tsx
@@ -1,12 +1,13 @@
 import { getInserate } from "@/actions/getInserate";
 import Header from "@/components/_searchpage/header";
+import InseratCard from "@/components/_searchpage/inserat-card";
 import { useEffect, useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 
 const MainPage = () => {
     console.log("MainPage");
     const [inserate, setInserate] = useState([]);
-    console.log(inserate)
+
 
     useEffect(() => {
         const load = async () => {
@@ -14,15 +15,24 @@ const MainPage = () => {
             setInserate(res);
         }
         load();
-    },[])
+    }, [])
 
-    return ( 
+    console.log(inserate.length)
+
+    return (
         <SafeAreaView className="flex-1  bg-[#1F2332]">
             <View className="">
-            <Header/>
+                <Header />
             </View>
+            <ScrollView className=" space-y-4">
+                {inserate.map((pInserat) => (
+                    <View key={pInserat.id}>
+                        <InseratCard thisInserat={pInserat} />
+                    </View>
+                ))}
+            </ScrollView>
         </SafeAreaView>
-     );
+    );
 }
- 
+
 export default MainPage;
