8491549000c2d62e49e2ed95b473ba5069949307
Pascal <zagarolopascal@gmail.com>
2024-07-26 22:19:00 +0200

    ..:

--- app/(tabs)/inserat/_components/inserat-attributes.tsx
+++ app/(tabs)/inserat/_components/inserat-attributes.tsx
@@ -4,6 +4,7 @@ import PkwAttributeRender from "./inserat-attributes/pkw-attributes";
 import { pkwAttribute } from '../../../../db/schema';
 import LkwAttributeRender from "./inserat-attributes/lkw-attributes";
 import TransportAttributeRender from "./inserat-attributes/transport-attributes";
+import TrailerAttributeRender from "./inserat-attributes/trailer-attributes";
 
 interface InseratAttributesProps {
     thisInserat : typeof inserat.$inferSelect
@@ -25,7 +26,8 @@ const InseratAttributes : React.FC<InseratAttributesProps> = ({
                     {
                         "PKW" : <PkwAttributeRender attributes={thisInserat?.pkwAttribute} />,
                         "LKW" : <LkwAttributeRender attributes={thisInserat?.lkwAttribute} />,
-                        "TRANSPORT" : <TransportAttributeRender attributes={thisInserat?.transportAttribute} />
+                        "TRANSPORT" : <TransportAttributeRender attributes={thisInserat?.transportAttribute} />,
+                        "TRAILER" : <TrailerAttributeRender attributes={thisInserat?.trailerAttribute} />
                     }[thisInserat?.category]
                 }                
             </View>
--- app/(tabs)/inserat/_components/inserat-attributes/trailer-attributes.tsx
+++ app/(tabs)/inserat/_components/inserat-attributes/trailer-attributes.tsx
@@ -23,33 +23,33 @@ const TrailerAttributeRender: React.FC<TrailerAttributeRenderProps> = ({
     return (
         <View className="w-full grid grid-cols-2 gap-2 mt-4 text-gray-200">
             {attributes?.type && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <MaterialCommunityIcons name="caravan" className="w-4 h-4 mr-2" />
-                    <Text>{attributes?.type?.substring(0, 1)}{attributes?.type?.substring(1).toLowerCase()}</Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <MaterialCommunityIcons name="caravan" className="w-4 h-4 mr-2" size={20} color="white"/>
+                    <Text className="text-gray-200 font-semibold">{attributes?.type?.substring(0, 1)}{attributes?.type?.substring(1).toLowerCase()}</Text>
                 </View>
             )}
             {attributes?.extraType && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <FontAwesome6 name="caravan" className="w-4 h-4 mr-2" />
-                    <Text>{attributes?.extraType?.substring(0, 1)}{attributes?.extraType?.substring(1).toLowerCase()}</Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <FontAwesome6 name="caravan" className="w-4 h-4 mr-2" size={20} color="white" />
+                    <Text className="text-gray-200 font-semibold">{attributes?.extraType?.substring(0, 1)}{attributes?.extraType?.substring(1).toLowerCase()}</Text>
                 </View>
             )}
             {attributes?.coupling && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <FontAwesome name="gear" className="w-4 h-4 mr-2" />
-                    <Text>{attributes?.coupling?.substring(0, 1)}{attributes?.coupling?.substring(1).toLowerCase()}</Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <FontAwesome name="gear" className="w-4 h-4 mr-2" size={20} color="white" />
+                    <Text className="text-gray-200 font-semibold">{attributes?.coupling?.substring(0, 1)}{attributes?.coupling?.substring(1).toLowerCase()}</Text>
                 </View>
             )}
             {attributes?.loading && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <MaterialCommunityIcons name="crane" className="w-4 h-4 mr-2" />
-                    <Text>{attributes?.loading?.substring(0, 1)}{attributes?.loading?.substring(1).toLowerCase()}</Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <MaterialCommunityIcons name="crane" className="w-4 h-4 mr-2" size={20} color="white" />
+                    <Text className="text-gray-200 font-semibold">{attributes?.loading?.substring(0, 1)}{attributes?.loading?.substring(1).toLowerCase()}</Text>
                 </View>
             )}
             {attributes?.axis && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <MaterialCommunityIcons name="axis" className="w-4 h-4 mr-2" />
-                    <Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <MaterialCommunityIcons name="axis" className="w-4 h-4 mr-2" size={20} color="white" />
+                    <Text className="text-gray-200 font-semibold" >
                         {{
                             '1': "Einachser",
                             '2': "Zweiachser",
@@ -61,9 +61,9 @@ const TrailerAttributeRender: React.FC<TrailerAttributeRenderProps> = ({
                 </View>
             )}
             {attributes?.weightClass && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <MaterialCommunityIcons name="weight" className="w-4 h-4 mr-2" />
-                    <Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <MaterialCommunityIcons name="weight" className="w-4 h-4 mr-2" size={20} color="white" />
+                    <Text className="text-gray-200 font-semibold">
                         {{
                             '75': " bis 0,75 t",
                             '150': " bis 1,5 t",
@@ -81,21 +81,21 @@ const TrailerAttributeRender: React.FC<TrailerAttributeRenderProps> = ({
                 </View>
             )}
             {attributes?.brake && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
-                    <MaterialCommunityIcons name="car-brake-worn-linings" className="w-4 h-4 mr-2" />
-                    <Text>{String(attributes?.brake) === "true" ? "Hat Auflaufbremse" : "Keine Bremse"}</Text>
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
+                    <MaterialCommunityIcons name="car-brake-worn-linings" className="w-4 h-4 mr-2" size={20} color="white" />
+                    <Text className="text-gray-200 font-semibold">{String(attributes?.brake) === "true" ? "Hat Auflaufbremse" : "Keine Bremse"}</Text>
                 </View>
             )}
             {attributes?.loading_volume && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
                     <Ionicons name="cube" className="w-4 h-4 mr-2 text-gray-200" size={20} color="white"/>
-                    <Text>{attributes.loading_volume} l</Text>
+                    <Text className="text-gray-200 font-semibold">{attributes.loading_volume} l</Text>
                 </View>
             )}
             {(attributes?.loading_l || attributes?.loading_b || attributes?.loading_h) && (
-                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center">
+                <View className="bg-[#13151C] p-4 font-semibold flex-row items-center space-x-4">
                     <Fontisto name="arrow-resize" className="w-4 h-4 mr-2" size={20} color="white" />
-                    <Text>{attributes?.loading_l} x {attributes?.loading_b} x {attributes?.loading_h} m</Text>
+                    <Text className="text-gray-200 font-semibold">{attributes?.loading_l} x {attributes?.loading_b} x {attributes?.loading_h} m</Text>
                 </View>
             )}
         </View>
