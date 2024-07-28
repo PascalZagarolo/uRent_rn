8497978aab70dd5dc284a853b466733e827ba9ce
Pascal <zagarolopascal@gmail.com>
2024-07-28 13:51:47 +0200

    toggle description..

--- app/(tabs)/profile/_components/profile-description.tsx
+++ app/(tabs)/profile/_components/profile-description.tsx
@@ -1,28 +1,44 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 interface ProfileDescriptionProps {
-    thisDescription : string;
-    thisName : string;
+    thisDescription: string;
+    thisName: string;
 }
 
-const ProfileDescription : React.FC<ProfileDescriptionProps> = ({
+const ProfileDescription: React.FC<ProfileDescriptionProps> = ({
     thisDescription,
     thisName
 }) => {
-    return ( 
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpanded = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const shouldShowMore = thisDescription.length > 100; 
+    const displayedText = isExpanded ? thisDescription : `${thisDescription.substring(0, 100)}...`;
+
+    return (
         <View className="p-4">
             <View>
                 <Text className="text-lg font-semibold text-gray-200">
-                über {thisName}
+                    über {thisName}
                 </Text>
             </View>
             <View>
                 <Text className="text-sm text-gray-200/90 font-medium">
-                {thisDescription}
+                    {displayedText}
                 </Text>
             </View>
+            <TouchableOpacity onPress={toggleExpanded} className="bg-[#1d1e2b] p-2 mt-2 rounded-md w-full flex justify-center">
+                <Text className="text-sm text-gray-200 font-semibold items-center flex justify-center text-center">
+                    {isExpanded ? "Weniger anzeigen" : "Mehr anzeigen"}
+                </Text>
+            </TouchableOpacity>
         </View>
-     );
+    );
 }
- 
+
 export default ProfileDescription;
