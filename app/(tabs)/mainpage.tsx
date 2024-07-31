1fc6b8837ca2bbe892a25f4a4622696f25f821e7
Pascal <zagarolopascal@gmail.com>
2024-08-01 00:03:27 +0200

    filter drawer..

--- app/(tabs)/mainpage.tsx
+++ app/(tabs)/mainpage.tsx
@@ -10,6 +10,7 @@ import { useAuth } from "./AuthProvider";
 import FilterBubbles from "@/components/_searchpage/_components/filter-bubbles";
 import { Drawer } from 'react-native-drawer-layout';
 import DrawerContentProfile from "@/components/_drawercontent/drawer-content-profile";
+import DrawerSearchFilter from "@/components/_drawercontent/drawer-search-filter";
 
 
 
@@ -29,10 +30,17 @@ const MainPage = () => {
     const { currentUser, isLoading } = useAuth();
 
     const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+    const [isFilterVisible, setIsFilterVisible] = useState(false);
 
     const toggleDrawer = () => {
         setIsDrawerVisible(!isDrawerVisible);
+        setIsFilterVisible(false);
     };
+
+    const toggleFilter = () => {
+        setIsFilterVisible(!isFilterVisible);
+        setIsDrawerVisible(false);
+    }
 
     console.log(currentUser)
 
@@ -41,8 +49,23 @@ const MainPage = () => {
     }
 
     return (
-        <SafeAreaView className="flex-1  bg-[#1F2332]">
-<Drawer
+        <SafeAreaView className="flex-1  bg-[#1F2332] w-full">
+            <Drawer
+                open={isFilterVisible}
+                onOpen={() => { setIsFilterVisible(true) }}
+                onClose={() => { setIsFilterVisible(false) }}
+                drawerPosition="left"
+                drawerType="slide"
+                drawerStyle={{ width: '100%' }}
+                renderDrawerContent={() => {
+                    return (
+                        <DrawerSearchFilter
+                            toggleFilter={toggleFilter}
+                        />
+                    )
+                }}
+            >
+                <Drawer
                     open={isDrawerVisible}
                     onOpen={() => { setIsDrawerVisible(true) }}
                     onClose={() => { setIsDrawerVisible(false) }}
@@ -50,39 +73,41 @@ const MainPage = () => {
                     drawerType="front"
                     renderDrawerContent={() => {
                         return (
-                            <DrawerContentProfile 
-                            currentUser = {currentUser}
+                            <DrawerContentProfile
+                                currentUser={currentUser}
                             />
                         )
                     }}
                 >
-            <ScrollView className=" ">
-                
-                
-                   
-                    <View className="">
-                        <Header
-                        toggleDrawer={toggleDrawer}
-                            currentUser={currentUser}
-                        />
-                    </View>
-                    <View className="p-2">
-                        <FilterBubbles
-                            currentResults={inserate.length}
-                        />
-                    </View>
-                    {inserate.map((pInserat) => (
-                        <View key={pInserat.id} className="border-t border-b border-gray-800 mb-2">
-                            <InseratCard thisInserat={pInserat} />
+                    <ScrollView className=" ">
+
+
+
+                        <View className="">
+                            <Header
+                                toggleDrawer={toggleDrawer}
+                                currentUser={currentUser}
+                            />
                         </View>
-                    ))}
-                    <View className="mt-4">
-                        <Footer />
-                    </View>
-                    
-                    
-                    
-            </ScrollView>
+                        <View className="p-2">
+                            <FilterBubbles
+                            toggleFilter={toggleFilter}
+                                currentResults={inserate.length as number}
+                            />
+                        </View>
+                        {inserate.map((pInserat) => (
+                            <View key={pInserat.id} className="border-t border-b border-gray-800 mb-2">
+                                <InseratCard thisInserat={pInserat} />
+                            </View>
+                        ))}
+                        <View className="mt-4">
+                            <Footer />
+                        </View>
+
+
+
+                    </ScrollView>
+                </Drawer>
             </Drawer>
         </SafeAreaView>
     );
---
+++ components/_drawercontent/drawer-search-filter.tsx
@@ -0,0 +1,56 @@
+import { AntDesign, FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather } from "lucide-react-native";
+import { Text } from "react-native";
+import { View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+interface DrawerSearchFilterProps {
+    toggleFilter: () => void;
+}
+
+
+const DrawerSearchFilter : React.FC<DrawerSearchFilterProps> = ({
+    toggleFilter
+}) => {
+    return (
+        <View className="bg-[#1F2332] p-4  border-gray-600 h-full ">
+            <View>
+                <View className="ml-auto ">
+                    <TouchableOpacity onPress={toggleFilter}>
+                        <FontAwesome name="close" size={24} color="#fff" />
+                    </TouchableOpacity>
+                </View>
+                <View className="flex flex-row items-center w-full justify-center">
+                    <Text className="text-gray-200 text-2xl font-semibold">
+                        Suchfilter
+                    </Text>
+                    
+                        
+                    
+                </View>
+                <View className="mt-2">
+                    <View className="flex flex-row items-center space-x-2">
+                    <View className="w-1/12">
+                        <MaterialCommunityIcons name="delete" size={24} color="#fff" />
+                        </View>
+                        <Text className="text-gray-200/90 text-sm">
+                            Filter zurücksetzen
+                        </Text>
+                    </View>
+                </View>
+                <View className="mt-2">
+                <View className="flex flex-row items-center space-x-2">
+                        <View className="w-1/12">
+                        <MaterialCommunityIcons name="content-save" size={24} color="#fff" />
+                        </View>
+                        <Text className="text-gray-200/90 text-sm">
+                            Suche speichern
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default DrawerSearchFilter;
