3e4d8babec85cf2c19d00cfc62992577d0c92b36
Pascal <zagarolopascal@gmail.com>
2024-07-31 17:58:15 +0200

    functioning login component..

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,3 +1,32 @@
-export async function getCurrentUser() {
-    
+'use server'
+'use strict'
+
+import db from "@/db/drizzle";
+import { userTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import JWT from "expo-jwt";
+
+export async function getCurrentUser(jwtString : string) {
+    try {
+        const decodedToken = await JWT.decode(jwtString, "77375149353387154508860974358780");
+        console.log(decodedToken);
+
+        if(!decodedToken.userId) {
+            return false;
+        }
+
+        const retrievedUser = await db.query.userTable.findFirst({
+            where : eq(
+                userTable.id, decodedToken.userId
+            )
+        })
+
+        
+
+        return retrievedUser;
+
+    } catch(e : any) {
+        console.log(e);
+        return false;
+    } 
 }
---
+++ app/(tabs)/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { View } from "react-native";
+import * as SecureStore from 'expo-secure-store';
+import { getCurrentUser } from "@/actions/getCurrentUser";
+
+const AuthProviderContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+
+    const [currentUser, setCurrentUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const retrieveCurrentUser = async () => {
+            try {
+                const foundToken = await SecureStore.getItemAsync("authToken");
+                if (foundToken) {
+                    const res = await getCurrentUser(foundToken);
+                    setCurrentUser(res);
+                }
+            } catch (error) {
+                console.error("Failed to retrieve current user:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        retrieveCurrentUser();
+    }, [])
+
+    return (
+        <AuthProviderContext.Provider value={{currentUser , setCurrentUser, isLoading}}>
+            {children}
+        </AuthProviderContext.Provider>
+    );
+}
+
+export const useAuth = () => useContext(AuthProviderContext);
