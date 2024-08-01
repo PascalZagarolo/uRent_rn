eab621ce721c48fb8ce3f60602dc54e7af294214
Pascal <zagarolopascal@gmail.com>
2024-08-02 00:05:55 +0200

    miet-ui

--- components/_drawercontent/_filter-content/_general-filter/select-date-filter.tsx
+++ components/_drawercontent/_filter-content/_general-filter/select-date-filter.tsx
@@ -1,18 +1,56 @@
-import { FontAwesome } from "@expo/vector-icons";
-import { Text, View } from "react-native";
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import Popover from 'react-native-popover-view';
 
 const SelectDateFilter = () => {
     return ( 
-        <View className="p-4 bg-[#171923]">
-            <View className="flex flex-row items-center gap-x-4">
+        <View>
+            <View className="">
+            <View className="flex flex-row items-center gap-x-4 p-4 bg-[#171923]">
                 <FontAwesome name="calendar" size={24} color="#fff" />
                 <Text className="text-lg font-semibold text-gray-200">
                     Mietzeitraum
                 </Text>
+                
             </View>
-            <View className="flex flex-row items-center">
-
+            <View className="flex flex-row w-full mt-2 justify-center space-x-4 px-8">
+                    <View className="w-1/2">
+                        <Text className="text-base font-semibold text-gray-200">
+                            Mietbeginn
+                        </Text>
+                        <Popover 
+      from={(
+        <TouchableOpacity className="w-full bg-[#1c1f2b] p-4 rounded-md flex flex-row">
+          <Text className="text-base text-gray-200">Start</Text>
+          <View className="ml-auto justify-end">
+          <Feather name="calendar" size={24} color="gray"  />
+          </View>
+        </TouchableOpacity>
+      )}>
+        <Text>This is the contents of the popover</Text>
+    </Popover>
+                    </View>
+                    <View className="w-1/2">
+                        <Text className="text-base font-semibold text-gray-200">
+                            Mietende
+                        </Text>
+                        <View className="flex flex-row w-full">
+                        <Popover 
+      from={(
+        <TouchableOpacity className="w-full bg-[#1c1f2b] p-4 rounded-md flex flex-row">
+          <Text className="text-base text-gray-200">Ende</Text>
+          <View className="ml-auto justify-end">
+          <Feather name="calendar" size={24} color="gray"  />
+          </View>
+        </TouchableOpacity>
+      )}>
+        <Text>This is the contents of the popover</Text>
+    </Popover>
+                        </View>
+                    </View>
             </View>
+        </View>
         </View>
      );
 }
