9f4fac8947256687e59c9e7b80a55568806a3eec
Pascal <zagarolopascal@gmail.com>
2024-08-03 17:26:03 +0200

    fuel

--- components/_drawercontent/_filter-content/_general-filter/_category/pkw-attributes-render.tsx
+++ components/_drawercontent/_filter-content/_general-filter/_category/pkw-attributes-render.tsx
@@ -2,6 +2,7 @@ import { View } from "react-native";
 import BrandTypeFilter from "../../_pkw-filter/brand-and-type";
 import SeatsFilter from "../../_pkw-filter/seats-filter";
 import AhkFilter from "../../_pkw-filter/ahk-filter";
+import FuelFilter from "../../_pkw-filter/fuel-filter";
 
 const PkwAttributeRender = () => {
     return ( 
@@ -14,6 +15,11 @@ const PkwAttributeRender = () => {
             </View>
             <View>
                 <AhkFilter />
+            </View>
+            <View className="flex items-center flex-row">
+                <View className="w-1/2">
+                    <FuelFilter />
+                </View>
             </View>
         </View>
      );
