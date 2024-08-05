8f4e28503c661c58e77e21c39706da4461ef51ff
Pascal <zagarolopascal@gmail.com>
2024-08-05 12:10:48 +0200

    filter bubbles..

--- components/_drawercontent/_filter-content/results.tsx
+++ components/_drawercontent/_filter-content/results.tsx
@@ -4,7 +4,8 @@ import { FontAwesome } from "@expo/vector-icons";
 import { useEffect, useMemo, useState } from "react";
 import { Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-
+import qs from "query-string";
+import { useRouter } from "expo-router";
 
 interface ResultProps {
     currentResults : number
@@ -13,6 +14,8 @@ interface ResultProps {
 const Results : React.FC<ResultProps> = ({
     currentResults
 }) => {
+    
+
     
 
     const [currentResult, setCurrentResult] = useState<number | null>();
@@ -28,9 +31,61 @@ const Results : React.FC<ResultProps> = ({
         getSearchResults();
     },[searchParams])
 
+    const router = useRouter();
+
+    const onRedirect = () => {
+        
+
+        const {//@ts-ignore
+            thisCategory, ...filteredValues} = searchParams;
+
+            
+        //@ts-ignore
+        const usedStart = filteredValues.periodBegin;
+
+        let usedEnd = null;
+        
+        
+    //@ts-ignore
+        if(filteredValues.periodEnd){
+        //@ts-ignore
+        usedEnd = filteredValues.periodEnd;
+        } else {
+            //@ts-ignore
+            if(filteredValues.periodBegin) {
+                //@ts-ignore
+                usedEnd = filteredValues.periodBegin;
+            }
+        }
+        const url = qs.stringifyUrl({
+            url: '/mainpage',
+
+
+            //@ts-ignore
+            query: {
+                //@ts-ignore
+                category: thisCategory,
+                //@ts-ignore
+                periodBegin: usedStart ? usedStart : null,
+                //@ts-ignore
+                periodEnd: usedEnd ? usedEnd : null,
+                //@ts-ignore
+                type: filteredValues.thisType,
+                ...filteredValues
+            },
+
+        }, { skipEmptyString: true, skipNull: true })
+        
+        console.log(url)
+        
+        router.push(url);
+    }
+
     return (
         <View className="p-4 ">
-            <TouchableOpacity className="w-full bg-indigo-800 p-8 rounded-md border border-gray-800">
+            <TouchableOpacity className="w-full bg-indigo-800 p-8 rounded-md border border-gray-800"
+            onPress={onRedirect}
+            >
                 <View className="flex flex-row items-center justify-center space-x-4">
                     <FontAwesome name="search" size={24} color="#fff" />
                     <Text className="text-center text-base font-semibold text-gray-200">
--- components/_searchpage/_components/filter-bubbles.tsx
+++ components/_searchpage/_components/filter-bubbles.tsx
@@ -1,26 +1,34 @@
+import { getSearchParamsFunction } from "@/actions/getSearchParams";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from "react-native";
+import ExistingFilterBubble from "./existing-filter-bubble";
 
 
 interface FilterBubblesProps {
-    currentResults : number;
-    toggleFilter : () => void;
+    currentResults: number;
+    toggleFilter: () => void;
 }
 
-const FilterBubbles : React.FC<FilterBubblesProps> = ({
+const FilterBubbles: React.FC<FilterBubblesProps> = ({
     currentResults,
     toggleFilter
 }) => {
-    return ( 
+
+    const params = getSearchParamsFunction();
+
+    
+
+    return (
         <View className="pb-2">
             <View>
                 <Text className="text-base font-semibold text-gray-200">
                     {currentResults} Inserate gefunden
                 </Text>
             </View>
-            <View className="flex flex-row mt-2">
+            
+            <View className="flex flex-row  items-center mr-2">
                 <TouchableOpacity className="flex flex-row items-center space-x-1 bg-indigo-800 p-2 rounded-md"
-                onPress={toggleFilter}
+                    onPress={toggleFilter}
                 >
                     <AntDesign name="plussquare" size={20} color={"white"} />
                     <Text className="text-gray-200 font-semibold">
@@ -28,8 +36,22 @@ const FilterBubbles : React.FC<FilterBubblesProps> = ({
                     </Text>
                 </TouchableOpacity>
             </View>
+            <View className="flex flex-row gap-x-2 w-full flex-wrap">
+                {Object.entries(params)
+                    .filter(([key, value]) => value !== null && value !== undefined)
+                    .map(([key, value]) => (
+                        <View className="mt-2">
+                            <ExistingFilterBubble
+                            key={key}
+                            pKey={key}
+                            value={value as string}
+                        />
+                        </View>
+                    ))}
+           
+            </View>
         </View>
-     );
+    );
 }
- 
+
 export default FilterBubbles;
