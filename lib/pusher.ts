b5984442b7fec2a186067eae541b27bf0d1636e8
Pascal <zagarolopascal@gmail.com>
2024-08-11 19:23:50 +0200

    integrated express..

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -3,22 +3,3 @@
 //https://codesandbox.io/s/pusher-demo-kin46x?file=/src/index.js
 //https://codesandbox.io/s/pusher-demo-kin46x?file=/src/index.js
 
-import { Pusher } from "@pusher/pusher-websocket-react-native";
-
-
-const pusher = Pusher.getInstance()
-
-export const connectPusher = async () => {
-    try {
-      await pusher.init({
-        apiKey : process.env.PUSHER_APP_KEY,
-        cluster : "eu",
-        authEndpoint : ""
-      })
-      await pusher.connect();
-
-      return pusher;
-    } catch(e : any) {
-      console.log(e);
-    }
-}
