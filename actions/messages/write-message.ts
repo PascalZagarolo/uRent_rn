f481e22291cced09353493943db13cf402ba8ff9
Pascal <zagarolopascal@gmail.com>
2024-08-11 14:53:17 +0200

    ..

--- actions/messages/write-message.ts
+++ actions/messages/write-message.ts
@@ -4,10 +4,12 @@ import db from "@/db/drizzle"
 import { conversation, message } from "@/db/schema"
 import { eq } from "drizzle-orm"
 import { getCurrentUser } from "../getCurrentUser"
-import { pusherServer } from "@/lib/pusher"
+
 
 export async function writeMessage(values : any) {
     try {
+
+        var Pusher = require('pusher');
 
         const currentUser = await getCurrentUser(values.token);
 
@@ -26,7 +28,8 @@ export async function writeMessage(values : any) {
         if(!foundConversation || !(foundConversation.user1Id === currentUser.id || foundConversation.user2Id === currentUser.id)) {
             return null;
         }
-        
+    
+        //@ts-ignore
         const writtenMessage = await db.insert(message).values({
             senderId : currentUser.id,
             conversationId : values.conversationId,
@@ -34,7 +37,15 @@ export async function writeMessage(values : any) {
             image : values?.image ? values.image : null
         }).returning();
 
-        await pusherServer.trigger(values.conversationId, 'messages:new', writtenMessage);
+        var pusher = new Pusher({
+            appId : process.env.EXPO_PUBLIC_PUSHER_APP_ID,
+            key : process.env.EXPO_PUBLIC_PUSHER_APP_KEY,
+            secret : process.env.EXPO_PUBLIC_PUSHER_APP_SECRET,
+            cluster : "eu"
+        })
+
+        pusher.trigger("messages", "new-massage", { writtenMessage})
+        
 
         return writtenMessage;
 
