5e58fa41cc96f80c5c899a483eed70c8446cd847
Pascal <zagarolopascal@gmail.com>
2024-08-20 17:04:25 +0200

    ...

--- actions/messages/write-message.ts
+++ actions/messages/write-message.ts
@@ -2,64 +2,120 @@
 'use server'
 
 import db from "@/db/drizzle"
-import { conversation, message } from "@/db/schema"
-import { eq } from "drizzle-orm"
+import { conversation, message, notification } from "@/db/schema"
+import { and, eq } from "drizzle-orm"
 import { getCurrentUser } from "../getCurrentUser"
-import {socket} from "@/lib/utils/socketService"
+import { socket } from "@/lib/utils/socketService"
 import axios from "axios"
 
-export async function writeMessage(values : any) {
+export async function writeMessage(values: any) {
     try {
 
-        
+
 
         const currentUser = await getCurrentUser(values.token);
 
-        if(!currentUser) {
+        if (!currentUser) {
             return null;
         }
 
         const findConversation = db.query.conversation.findFirst({
-            where : eq(
+            where: eq(
                 conversation.id, values.conversationId
             )
         }).prepare("findConversation");
 
         const foundConversation = await findConversation.execute();
 
-        if(!foundConversation || !(foundConversation.user1Id === currentUser.id || foundConversation.user2Id === currentUser.id)) {
+        if (!foundConversation || !(foundConversation.user1Id === currentUser.id || foundConversation.user2Id === currentUser.id)) {
             return null;
         }
-    
-        //@ts-ignore
-        const [writtenMessage] : any = await db.insert(message).values({
-            senderId : currentUser.id,
-            conversationId : values.conversationId,
-            content : values?.content ? values.content : null,
-            image : values?.image ? values.image : null
-        }).returning();
 
-        console.log(writtenMessage);
-
-        
-        const updateLastMessage = await db.update(conversation).set({
-            //@ts-ignore
-            message : writtenMessage,
-            lastMessageId : writtenMessage.id
-        }).where(eq(conversation.id, values.conversationId))
-        
-        
-        await axios.post(`https://www.urent-rental.de/api/public/${values.conversationId}/sent-message`, writtenMessage)
-       
-        socket.emit("newMessage", {
-            sendMessage : writtenMessage,
-            conversationId : values.conversationId
+        const existingMessage = await db.query.conversation.findFirst({
+            where : eq(
+                conversation.id, values.conversationId
+            )
         })
 
-        return writtenMessage;
+        const writtenMessage = await db.insert(message).values({
+            senderId: currentUser.id,
+            conversationId: values.conversationId,
+            content: values?.content ? values.content : null,
+            image: values?.image ? values.image : null
+        }).returning();
 
 
-    } catch(e : any){
+
+        
+
+        const updateLastMessage = await db.update(conversation)
+        .set({
+            message: writtenMessage[0], lastMessageId: writtenMessage[0].id
+        })
+        .where(eq(conversation.id, foundConversation.id))
+            
+
+        const existingNotification = await db.query.notification.findFirst({
+            where:
+                and(
+                    eq(notification.userId, currentUser.id),
+                    eq(notification.conversationId, values.conversationId),
+                    eq(notification.seen, false)
+                )
+        })
+
+        if(!existingNotification) {
+
+            const otherUserId = foundConversation.user1Id === currentUser?.id ? foundConversation?.user2Id : foundConversation?.user1Id
+
+            const [createdNotification] = await db.insert(notification).values({
+                userId : otherUserId,
+                conversationId : values.conversationId,
+                notificationType : "MESSAGE",
+                content : currentUser?.name
+            }).returning()
+
+
+
+            const parameters = {
+                sentMessage: writtenMessage[0],
+                sender : {
+                    id : currentUser?.id,
+                    name : currentUser?.name,
+                    image : currentUser?.image
+                },
+                createdNotification : createdNotification,
+                startedConversation : existingMessage ? true : false,
+                imageUrl : currentUser?.image
+            }
+
+            await axios.post(`https://www.urent-rental.de/api/public/${values.conversationId}/sent-message`, parameters)
+        } else {
+            const parameters = {
+                sentMessage: writtenMessage[0],
+                sender : {
+                    id : currentUser?.id,
+                    name : currentUser?.name,
+                    image : currentUser?.image
+                },
+            }
+            await axios.post(`https://www.urent-rental.de/api/public/${values.conversationId}/sent-message`, parameters)
+        }
+
+
+        
+
+        
+
+        socket.emit("newMessage", {
+            sendMessage: writtenMessage[0],
+            conversationId: values.conversationId
+        })
+
+        return writtenMessage[0];
+
+
+    } catch (e: any) {
         console.log(e)
     }
 }
