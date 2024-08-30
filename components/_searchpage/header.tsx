cdb279a0b186659c07b0f51a76a9e95087d9afdc
Pascal <zagarolopascal@gmail.com>
2024-08-31 00:37:53 +0200

    ....

--- components/_searchpage/header.tsx
+++ components/_searchpage/header.tsx
@@ -1,5 +1,5 @@
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, Modal, Text, TextInput, TouchableOpacity, View } from "react-native";
 import SearchBar from "./search-bar";
 import { userTable } from "@/db/schema";
 import { useRef, useState } from "react";
@@ -9,6 +9,7 @@ import { is } from "drizzle-orm";
 import { SafeAreaView } from "react-native";
 import { cn } from "@/~/lib/utils";
 import { router } from "expo-router";
+import CreateInseratPreview from "./_components/create-inserat-preview";
 interface HeaderProps {
   currentUser: typeof userTable.$inferSelect | any;
   toggleDrawer: () => void;
@@ -22,6 +23,7 @@ const Header: React.FC<HeaderProps> = ({ currentUser, toggleDrawer, toggleNotifi
   )
 
   const [showModal, setShowModal] = useState(false);
+ 
 
   const usedUrl = currentUser?.image
     ? currentUser.image
@@ -41,14 +43,14 @@ const Header: React.FC<HeaderProps> = ({ currentUser, toggleDrawer, toggleNotifi
         {currentUser ? (
 
           <View className="ml-auto flex flex-row items-center space-x-8">
-            <TouchableOpacity onPress={toggleNotifications}>
+            <TouchableOpacity onPress={() => {setShowModal(true)}}>
               <Ionicons name="add-circle-outline" size={24} color="white" />
-              
+
             </TouchableOpacity>
             <TouchableOpacity onPress={toggleNotifications}>
               <Ionicons name="notifications" size={24} color="white" />
-              <View className={cn("absolute -top-1.5 -right-1.5 rounded-full px-1 py-0.5", 
-              unseenNotifications === 0 ? "" : "bg-rose-600"
+              <View className={cn("absolute -top-1.5 -right-1.5 rounded-full px-1 py-0.5",
+                unseenNotifications === 0 ? "" : "bg-rose-600"
               )}>
                 <Text style={{ fontSize: 10, color: 'rgba(229, 231, 235, 0.9)' }}>{unseenNotifications}</Text>
               </View>
@@ -63,13 +65,25 @@ const Header: React.FC<HeaderProps> = ({ currentUser, toggleDrawer, toggleNotifi
           </View>
 
         ) : (
-          <TouchableOpacity className="ml-auto" onPress={() => { router.push("/login")}}>
+          <TouchableOpacity className="ml-auto" onPress={() => { router.push("/login") }}>
             <FontAwesome name="user-circle" size={24} color="white" />
           </TouchableOpacity>
         )}
 
       </SafeAreaView>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => {
+          setShowModal(false);
+        }}
 
+      >
+        <CreateInseratPreview 
+        closeModal={() => setShowModal(false)}
+        />
+      </Modal>
 
 
     </View>
