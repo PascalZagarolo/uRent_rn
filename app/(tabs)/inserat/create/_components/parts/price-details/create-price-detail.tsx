b1cec0b39e438bc3226f116a4e69bae45b4f6ac5
Pascal <zagarolopascal@gmail.com>
2024-09-02 00:26:02 +0200

    .

--- app/(tabs)/inserat/create/_components/parts/price-details/create-price-detail.tsx
+++ app/(tabs)/inserat/create/_components/parts/price-details/create-price-detail.tsx
@@ -1,20 +1,27 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from "react-native";
 
+import { useState } from "react";
+import { set } from 'date-fns';
+
 interface CreatePriceProfileProps {
     onClose: () => void;
 }
 
 const CreatePriceProfile = ({ onClose }: CreatePriceProfileProps) => {
-
-
+    const [currentTitle, setCurrentTitle] = useState(null);
+    const [currentPrice, setCurrentPrice] = useState(null);
+    const [currentFreemiles, setCurrentFreemiles] = useState(null);
+    const [currentExtraprice, setCurrentExtraprice] = useState(null);
+    const [currentDescription, setCurrentDescription] = useState(null);
+    
 
     return (
         <View className="flex-1 justify-center items-center bg-black/80">
             <View className="bg-[#151821] w-full rounded-lg overflow-hiddens">
                 <View className="flex flex-row items-center p-4">
                     <Text className="text-xl font-semibold text-gray-200">
-                        Inserat erstellen
+                        Preisprofil hinzufügen
                     </Text>
                     <TouchableOpacity className="ml-auto">
                         <FontAwesome name="close" size={24} color="white" onPress={onClose} />
