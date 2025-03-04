19d2e950b7d53833d82d9a5302d1892f4408d622
Pascal <zagarolopascal@gmail.com>
2025-03-04 22:13:30 +0100

    ...

--- app/(tabs)/inserat/_components/inserat-profile.tsx
+++ app/(tabs)/inserat/_components/inserat-profile.tsx
@@ -1,5 +1,5 @@
 import { business, userTable } from "@/db/schema";
-import { Feather, FontAwesome, FontAwesome5, Foundation, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { Feather, FontAwesome, FontAwesome5, Foundation, Ionicons, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { format } from "date-fns/format";
 import { useRouter } from "expo-router";
 
@@ -8,12 +8,12 @@ import { businessAddress } from '../../../../db/schema';
 import placeholderPicture from "@/assets/images";
 
 interface InseratProfileProps {
-    thisUser : typeof userTable.$inferSelect & { inserat, business : typeof business.$inferSelect & { businessAddresses : typeof businessAddress.$inferSelect[] } };
-    
+    thisUser: typeof userTable.$inferSelect & { inserat, business: typeof business.$inferSelect & { businessAddresses: typeof businessAddress.$inferSelect[] } };
+
 }
 
 
-const InseratProfile : React.FC<InseratProfileProps> = ({
+const InseratProfile: React.FC<InseratProfileProps> = ({
     thisUser
 }) => {
 
@@ -25,23 +25,23 @@ const InseratProfile : React.FC<InseratProfileProps> = ({
 
     const router = useRouter();
 
-    return ( 
+    return (
         <View className="px-2 ">
             <View className="flex flex-row items-center gap-x-4">
-            <Image source={{uri: thisUser.image ?? placeholderPicture }}  
-                className="w-8 h-8 rounded-full"
+                <Image source={{ uri: thisUser.image ?? placeholderPicture }}
+                    className="w-10 h-10 rounded-full"
                 />
-            <Text className="text-base font-semibold w-3/4 text-gray-200 break-all line-clamp-1" numberOfLines={1}>
-                {thisUser.name} 
-            </Text>
+                <Text className="text-lg font-semibold w-3/4 text-gray-200 break-all line-clamp-1" numberOfLines={1}>
+                    {thisUser.name}
+                </Text>
             </View>
             <View className=" mt-2 space-y-2">
-            <View className="flex flex-row items-center gap-x-2">
-                        
-                        <Text className="text-gray-200/80 font-semibold break-all line-clamp-1 text-md" numberOfLines={1}>
-                            {publicInserate.length} Inserate online
-                        </Text>
-                    </View>
+                <View className="flex flex-row items-center gap-x-2">
+
+                    <Text className="text-gray-200/80 font-semibold break-all line-clamp-1 text-md" numberOfLines={1}>
+                        {publicInserate.length} Inserate online
+                    </Text>
+                </View>
                 {(thisUser?.sharesRealName && (thisUser?.vorname || thisUser?.nachname)) && (
                     <View className="flex flex-row items-center gap-x-4">
                         <FontAwesome name="user-circle" size={16} color="white" />
@@ -60,89 +60,106 @@ const InseratProfile : React.FC<InseratProfileProps> = ({
                 )}
                 {thisUser?.createdAt && (
                     <View className="flex flex-row items-center gap-x-4">
-                        
+
                         <Text className="text-gray-200/60 text-xs font-medium break-all line-clamp-1" numberOfLines={1}>
                             Mitglied seit: {format(new Date(thisUser.createdAt), "dd.MM.yyyy")}
                         </Text>
                     </View>
                 )}
-{thisUser?.isBusiness && (
-                    <View className=" w-full  border-gray-800">
+                {thisUser?.isBusiness && (
+                    <View className=" w-full  border-gray-800 ">
                         {thisUser?.business.businessAddresses?.length > 0 && (
-                            <View>
+                            <View className="mt-4">
                                 <View className="flex flex-row items-center gap-x-4">
                                     <FontAwesome name="map-marker" size={16} color="red" />
                                     <Text className="text-md font-semibold text-gray-200">
-                                    Firmenstandort
+                                        Firmenstandort
                                     </Text>
                                 </View>
                                 <View className="mt-2">
-                                    <Image
-                                    source={{uri: foundMainAddress[0].image }}
-                                    className="w-full h-32 object-cover"
-                                    />
+                                    {foundMainAddress[0].image ? (
+                                        <Image
+                                            source={{ uri: foundMainAddress[0].image }}
+                                            className="w-full h-32 object-cover shadow-lg rounded-md"
+                                        />
+                                    ) : (
+                                        <View
+                                            className="h-32 flex flex-row items-center justify-center"
+                                        >
+                                            <MaterialCommunityIcons name="image" size={20} color={"white"} />
+                                        </View>
+                                    )}
                                     <Text className="mt-2 ml-auto text-sm font-medium text-gray-200">
-                                    {foundMainAddress[0]?.street ? foundMainAddress[0]?.street + ", " : ""} 
-                                    {foundMainAddress[0]?.postalCode} {foundMainAddress[0]?.city}
+                                        {foundMainAddress[0]?.street ? foundMainAddress[0]?.street + ", " : ""}
+                                        {foundMainAddress[0]?.postalCode} {foundMainAddress[0]?.city}
                                     </Text>
                                 </View>
-                            </View>  
+                            </View>
                         )}
                         <View className="py-4">
-                            <View className="w-full mb-4">
-                                <Text className="text-md font-semibold text-gray-200">
-                                    Weitere Kontaktdaten
+                            {(thisUser?.business?.website || thisUser?.business?.email || thisUser?.business?.telephone_number || thisUser?.business?.fax) && (
+                                <View className="w-full ">
+                                <Text className="text-base font-semibold text-gray-200">
+                                    Kontaktdaten
                                 </Text>
                             </View>
-                            <View className="flex flex-row items-center">
-                                <View className="w-2/6 flex flex-row items-center gap-x-4">
-                                    
-                                    <Feather name="globe" size={16} color="white" />
-                                    <Text className="text-md font-medium text-gray-200/80">
-                                    Website
-                                    </Text>
-                                </View>
-                                <View className="w-5/6">
-                                    <Text className="text-md font-medium text-gray-200 ">
-                                        {thisUser?.business?.website ? thisUser?.business?.website : "-"}
-                                    </Text>
-                                </View>
-                            </View>
+                            )}
+                            {thisUser?.business?.website && (
+                                <View className="flex flex-row items-center mt-2">
+                                    <View className="w-2/6 flex flex-row items-center gap-x-4">
 
-                            <View className="flex flex-row items-center mt-4">
-                                <View className="w-2/6 flex flex-row items-center gap-x-4">
-                                    
-                                    <View>
-                                    <Ionicons name="mail-unread" size={16} color="white" />
+                                        <Feather name="globe" size={16} color="white" />
+                                        <Text className="text-md font-medium text-gray-200/80">
+                                            Website
+                                        </Text>
                                     </View>
-                                    <Text className="text-md font-medium text-gray-200/80">
-                                    Email
-                                    </Text>
+                                    <View className="w-5/6">
+                                        <Text className="text-md font-medium text-gray-200 ">
+                                            {thisUser?.business?.website ? thisUser?.business?.website : "-"}
+                                        </Text>
+                                    </View>
                                 </View>
-                                <View className="w-4/6">
-                                    <Text className="text-md font-medium text-gray-200">
-                                        {thisUser?.business?.email ? thisUser?.business?.email : "-"}
-                                    </Text>
+                            )}
+
+                            {thisUser?.business?.email && (
+                                <View className="flex flex-row items-center mt-2">
+                                    <View className="w-2/6 flex flex-row items-center gap-x-4">
+
+                                        <View>
+                                            <Ionicons name="mail-unread" size={16} color="white" />
+                                        </View>
+                                        <Text className="text-md font-medium text-gray-200/80">
+                                            Email
+                                        </Text>
+                                    </View>
+                                    <View className="w-4/6">
+                                        <Text className="text-md font-medium text-gray-200">
+                                            {thisUser?.business?.email ? thisUser?.business?.email : "-"}
+                                        </Text>
+                                    </View>
                                 </View>
-                            </View>
-                            
-                            <View className="flex flex-row items-center mt-4">
+                            )}
+
+                            {thisUser?.business?.telephone_number && (
+                                <View className="flex flex-row items-center mt-2">
+                                    <View className="w-2/6 flex flex-row items-center gap-x-4">
+
+                                        <FontAwesome name="phone-square" size={16} color="white" />
+                                        <Text className="text-md font-medium text-gray-200/80">
+                                            Telefon
+                                        </Text>
+                                    </View>
+                                    <View className="w-4/6">
+                                        <Text className="text-md font-medium text-gray-200/80">
+                                            {thisUser?.business?.telephone_number ? thisUser?.business?.telephone_number : "-"}
+                                        </Text>
+                                    </View>
+                                </View>
+                            )}
+                            {thisUser?.business?.fax && (
+                                <View className="flex flex-row items-center mt-2">
                                 <View className="w-2/6 flex flex-row items-center gap-x-4">
-                                    
-                                <FontAwesome name="phone-square" size={16} color="white" />
-                                    <Text className="text-md font-medium text-gray-200/80">
-                                        Telefon
-                                    </Text>
-                                </View>
-                                <View className="w-4/6">
-                                <Text className="text-md font-medium text-gray-200/80">
-                                        {thisUser?.business?.telephone_number ? thisUser?.business?.telephone_number : "-"}
-                                    </Text>
-                                </View>
-                            </View>
-                            <View className="flex flex-row items-center mt-4">
-                                <View className="w-2/6 flex flex-row items-center gap-x-4">
-                                    
+
                                     <FontAwesome5 name="fax" size={16} color="white" />
                                     <Text className="text-md font-medium text-gray-200/80">
                                         Fax
@@ -154,19 +171,22 @@ const InseratProfile : React.FC<InseratProfileProps> = ({
                                     </Text>
                                 </View>
                             </View>
-                        </View>
+                            
+                            )}
+                            
+                            </View>
                     </View>
-)}
-                <TouchableOpacity onPress={() => {router.push(`/profile/${thisUser.id}`)}} className="bg-[#242635] 
+                )}
+                <TouchableOpacity onPress={() => { router.push(`/profile/${thisUser.id}`) }} className="bg-[#242635] 
                 p-2 mt-2 rounded-md w-full flex justify-center flex-row space-x-4">
                     <FontAwesome name="user" size={16} color="white" />
-                        <Text className="text-sm text-gray-200 font-semibold items-center flex justify-center text-center">
-                            Zum Profil
-                        </Text>
+                    <Text className="text-sm text-gray-200 font-semibold items-center flex justify-center text-center">
+                        Zum Profil
+                    </Text>
                 </TouchableOpacity>
             </View>
         </View>
-     );
+    );
 }
- 
+
 export default InseratProfile;
